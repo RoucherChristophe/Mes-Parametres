b907643703b338796d57f827520b0782b9d935de
‘RoucherChristophe’ <c.roucher@orange.fr>
2021-04-09 11:50:14 +0200

    Classe Helper: insert,update,delete

--- app/src/main/java/com/example/mesparametres/Helper.java
+++ app/src/main/java/com/example/mesparametres/Helper.java
@@ -1,5 +1,6 @@
 package com.example.mesparametres;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -30,4 +31,37 @@ public class Helper extends SQLiteOpenHelper {
         this.onCreate( db );
         Log.i( "DATABASE", "onUpgrade invoked" );
     }
+
+    //**********************************************************************************************
+    public  void insertValid (ValidCheck val) {
+        // pour écrire dans la BDD
+        SQLiteDatabase db = this.getWritableDatabase();
+        // pour inserer les données
+        ContentValues cv = new ContentValues();
+        cv.put("nomParam", val.getNomParam());
+        cv.put("valid", val.getValid());
+
+        db.insert("ValidParam", null, cv);
+        // on ferme la connexion
+        db.close();
+    }
+
+    public void updateHoraire(ValidCheck val) {
+        // instance, on met le privilège en écriture
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+        cv.put("nomParam", val.getNomParam());
+        cv.put("valid", val.getValid());
+
+        db.update("ValidParam", cv,"_id=?",new String[]{String.valueOf(val.getId())});
+        // on ferme la connexion
+        db.close();
+    }
+
+    public void deleteDatabase() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String clearDBQuery = "DELETE FROM ValidParam" ;
+        db.execSQL(clearDBQuery); }
+
 }
